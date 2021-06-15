7888ad5320d940e9a9c3bff40db49065a2fee16c
zyhibook <zyh15637540388@gmail.com>
2021-06-15 10:14:27 +0800

    更新README.md

--- FFmpegApp/Form1.Designer.cs
+++ FFmpegApp/Form1.Designer.cs
@@ -147,7 +147,6 @@ namespace FFmpegApp
             this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.pictureBox1.Image = global::FFmpegApp.Properties.Resources.wallhaven_96yeo1;
             this.pictureBox1.Location = new System.Drawing.Point(12, 101);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(776, 337);
