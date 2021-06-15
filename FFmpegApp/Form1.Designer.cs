76ca2ad8cf9659b6d1d354e7f1d34a625a7336d0
zyhibook <zyh15637540388@gmail.com>
2021-06-15 08:48:21 +0800

    添加项目文件。

---
+++ FFmpegApp/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace FFmpegApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.labelIn = new System.Windows.Forms.Label();
+            this.labelOut = new System.Windows.Forms.Label();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.labelP = new System.Windows.Forms.Label();
+            this.buttonOpenPath = new System.Windows.Forms.Button();
+            this.buttonSavePath = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStart.Location = new System.Drawing.Point(713, 10);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "开始";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(45, 10);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(581, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(45, 72);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(581, 23);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.Location = new System.Drawing.Point(45, 41);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(581, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // labelIn
+            // 
+            this.labelIn.AutoSize = true;
+            this.labelIn.Location = new System.Drawing.Point(10, 16);
+            this.labelIn.Name = "labelIn";
+            this.labelIn.Size = new System.Drawing.Size(29, 12);
+            this.labelIn.TabIndex = 4;
+            this.labelIn.Text = "输入";
+            // 
+            // labelOut
+            // 
+            this.labelOut.AutoSize = true;
+            this.labelOut.Location = new System.Drawing.Point(10, 44);
+            this.labelOut.Name = "labelOut";
+            this.labelOut.Size = new System.Drawing.Size(29, 12);
+            this.labelOut.TabIndex = 5;
+            this.labelOut.Text = "输出";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonStop.Location = new System.Drawing.Point(713, 39);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 6;
+            this.buttonStop.Text = "停止";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            // 
+            // labelP
+            // 
+            this.labelP.AutoSize = true;
+            this.labelP.Location = new System.Drawing.Point(10, 72);
+            this.labelP.Name = "labelP";
+            this.labelP.Size = new System.Drawing.Size(29, 12);
+            this.labelP.TabIndex = 7;
+            this.labelP.Text = "进度";
+            // 
+            // buttonOpenPath
+            // 
+            this.buttonOpenPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOpenPath.Location = new System.Drawing.Point(632, 10);
+            this.buttonOpenPath.Name = "buttonOpenPath";
+            this.buttonOpenPath.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenPath.TabIndex = 8;
+            this.buttonOpenPath.Text = "浏览";
+            this.buttonOpenPath.UseVisualStyleBackColor = true;
+            this.buttonOpenPath.Click += new System.EventHandler(this.buttonOpenPath_Click);
+            // 
+            // buttonSavePath
+            // 
+            this.buttonSavePath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSavePath.Location = new System.Drawing.Point(632, 39);
+            this.buttonSavePath.Name = "buttonSavePath";
+            this.buttonSavePath.Size = new System.Drawing.Size(75, 23);
+            this.buttonSavePath.TabIndex = 9;
+            this.buttonSavePath.Text = "保存路径";
+            this.buttonSavePath.UseVisualStyleBackColor = true;
+            this.buttonSavePath.Click += new System.EventHandler(this.buttonSavePath_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::FFmpegApp.Properties.Resources.wallhaven_96yeo1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 101);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 337);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSavePath);
+            this.Controls.Add(this.buttonOpenPath);
+            this.Controls.Add(this.labelP);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.labelOut);
+            this.Controls.Add(this.labelIn);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "视频处理工具箱 by:zhi v0.0.9";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label labelIn;
+        private System.Windows.Forms.Label labelOut;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Label labelP;
+        private System.Windows.Forms.Button buttonOpenPath;
+        private System.Windows.Forms.Button buttonSavePath;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ FFmpegApp/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace FFmpegApp
+{
+    public partial class Form1 : Form
+    {
+        Process FProcess;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void buttonOpenPath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog f = new OpenFileDialog();
+            f.Filter = "视频文件|*.mp4;*.avi;*.flv";
+            f.FilterIndex = 1;
+            DialogResult re = f.ShowDialog();
+            string FileOpenPath = f.FileName;
+            if (re == DialogResult.OK)
+            {
+                textBox1.Text = f.FileName;
+            }
+        }
+
+        private void buttonSavePath_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog s = new SaveFileDialog();
+            s.Filter= "视频文件|*.mp4;*.flv;*.avi|音频文件|*.mp3|图片文件|*.png;*.jpg;*.gif";
+            s.FilterIndex = 1;
+            DialogResult re = s.ShowDialog();
+            string FileSavePath = s.FileName;
+            if (re == DialogResult.OK)
+            {
+                textBox2.Text = s.FileName;
+            }
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            string Fpath = "ffmpeg.exe";
+            if (File.Exists(Fpath))
+            {
+                FProcess = new Process();
+                FProcess.StartInfo.FileName = "ffmpeg.exe";
+                FProcess.StartInfo.Arguments = $"-i {textBox1.Text} {textBox2.Text}";
+                FProcess.Start();
+            }
+            else
+            {
+                MessageBox.Show("出现错误！不能开始！因为当前目录下缺少ffmpeg.exe文件！");
+            }
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
